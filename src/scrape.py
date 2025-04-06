c4892c5e061d96a0de2e0e213969f67167e41b5e
Yashas <yashaspancham@gmail.com>
2025-04-06 22:15:22 +0530

    Yashas-fix-FixesAndAutomatedUpdatingrequirements.txt

--- src/scrape.py
+++ src/scrape.py
@@ -1,9 +1,7 @@
 import requests
-from bs4 import BeautifulSoup
 
 def webscrape(security_code:str)->str:
     address = "https://www.screener.in/company/" + security_code + "/"
     request = requests.get(address)
-    soup = BeautifulSoup(request.content, 'html.parser')
     html_data = request.content
     return html_data
---
+++ update_pip.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+pip "$@"
+pip freeze > requirements.txt
